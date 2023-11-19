85fc4f3a07b163a8366a7d7bf97f42e4fe9ea2cb
MySlientWind <634790985@qq.com>
2023-11-19 16:29:35 +0800

    get_seg_cmap

--- emilkit/utils.py
+++ emilkit/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
+import matplotlib.colors as mcolors
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
@@ -50,6 +51,37 @@ def imshow(data,
     im = ax.imshow(data, origin='lower', cmap=cmap, *arg, **kwarg)
     fig.colorbar(im, ax=ax, cax=cax, extend=extend)
     fig.sca(ax)  # 把当前 axis 设为 ax
+
+def get_seg_cmap(bins, under=None, over=None, colors=None):
+    """
+    Get a colormap and norm for a given set of bins
+    if colors is None, then the colors are linearly interpolated between under and over
+    """
+
+    nbin = len(bins) - 1
+    norm = mcolors.BoundaryNorm(bins, nbin)
+
+    # Calculate colors using linear interpolation
+    # sourcery skip: merge-nested-ifs
+    if colors is None:
+        if under is not None and over is not None:
+            num_colors = len(bins) + 1
+            # colors = [mcolors.to_rgb(under)]
+            colors = []
+            for i in range(1, num_colors - 1):
+                t = i / (num_colors - 1)
+                color = [
+                    t * c1 + (1 - t) * c2 for c1, c2 in zip(
+                        mcolors.to_rgb(over), mcolors.to_rgb(under))
+                ]
+                colors.append(color)
+            # colors.append(mcolors.to_rgb(over))
+
+    cmap = mcolors.ListedColormap(colors, name='Nsigma')
+    cmap.set_under(under)
+    cmap.set_over(over)
+
+    return cmap, norm
 
 def plot_polygon(pts, ax=None, *arg, **kwarg):
     if ax is None:
